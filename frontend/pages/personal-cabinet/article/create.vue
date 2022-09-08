304fed62c1df3abc735268ab907bae340bd2f8ac
FIRST_NAME LAST_NAME <skwidw92@gmail.com>
2022-09-08 11:23:27 +0300

    presonal_cabinet | add func-s for upload image

--- frontend/pages/personal-cabinet/article/create.vue
+++ frontend/pages/personal-cabinet/article/create.vue
@@ -5,6 +5,14 @@
       <label>Email: </label>
       <input v-model="form.title" type="text" name="name" :class="{ 'is-invalid': errors.title }" placeholder="title">
       <input v-model="form.description" type="text" name="description" :class="{ 'is-invalid': errors.description }" placeholder="description">
+
+      <input type="file" id="files" ref="files" accept="image/*" @change="handleFilesUpload()" multiple>
+      <div class="large-12 medium-12 small-12 cell">
+        <div v-for="(file, key) in files" class="file-listing">{{ file.name }}
+          <span class="remove-file" v-on:click="removeFile( key )">Remove</span>
+        </div>
+      </div>
+
       <div class="invalid-feedback" v-if="errors.text">
         {{ errors.text[0] }}
       </div>
@@ -18,6 +26,9 @@
 </template>
 
 <script>
+
+import _ from 'lodash'
+
 export default {
   name: "create",
   middleware: 'auth',
@@ -28,18 +39,30 @@ export default {
         description: '',
         author_id: this.$auth.user.id,
       },
+      files: [],
       error: this.$route.query.error
     }
   },
   methods: {
     async create() {
-      try {
-        await this.$axios.post('/article/store', this.form);
-      } catch(e) {
-        return;
+      let form = new FormData();
+      for( var i = 0; i < this.files.length; i++ ){
+        let file = this.files[i];
+        form.append('files[' + i + ']', file)
+        _.each(this.form, (value, key) => {
+          form.append(key, value)});
       }
 
-      this.$router.push({name: 'index'});
+      await this.$axios.post('/article/store', form, {})
+    },
+    handleFilesUpload() {
+      let uploadedFiles = this.$refs.files.files;
+      for( var i = 0; i < uploadedFiles.length; i++ ){
+        this.files.push( uploadedFiles[i] );
+      }
+    },
+    removeFile( key ) {
+      this.files.splice( key, 1 );
     }
   }
 }
